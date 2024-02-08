441d66fd81e5ccd7d9c70305369602483c315415
sriffaldi <stefano.riffaldi@gmail.com>
2024-02-08 12:39:00 +0100

    add another behavior at example

--- spring-boot-system-environment/src/main/java/org/example/MyApp.java
+++ spring-boot-system-environment/src/main/java/org/example/MyApp.java
@@ -1,25 +1,29 @@
-    package org.example;
+package org.example;
 
-    import lombok.extern.slf4j.Slf4j;
-    import org.springframework.beans.factory.annotation.Value;
-    import org.springframework.boot.ApplicationArguments;
-    import org.springframework.boot.ApplicationRunner;
-    import org.springframework.boot.SpringApplication;
-    import org.springframework.boot.autoconfigure.SpringBootApplication;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-    @Slf4j
-    @SpringBootApplication
-    public class MyApp implements ApplicationRunner {
+@Slf4j
+@SpringBootApplication
+public class MyApp implements ApplicationRunner {
 
-        @Value("#{systemEnvironment['HERO']}")
-        private String sysEnvField;
+    @Value("#{systemEnvironment['HERO']}")
+    private String sysEnvNotation;
 
-        @Override
-        public void run(ApplicationArguments args) throws Exception {
-            log.info("SystemEnvironment: {}", sysEnvField);
-        }
+    @Value("${HERO}")
+    private String simpleNotation;
 
-        public static void main(String[] args) {
-            SpringApplication.run(MyApp.class, args);
-        }
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        log.info("With    'systemEnvironment' notation : {}", sysEnvNotation);
+        log.info("Without 'systemEnvironment' notation : {}", simpleNotation);
     }
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyApp.class, args);
+    }
+}
